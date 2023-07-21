0bd9930dd595254b8f995f8670d9289c99291b3b
cmlee2 <cameronmlee.home@gmail.com>
2023-07-20 19:35:27 -0700

    Added Comments

--- Flask_API.py
+++ Flask_API.py
@@ -34,11 +34,14 @@ import json
 import pprint
 import requests
 
+# Load Client Secret and Client ID
 load_dotenv()
 
+# Load Client Secret and Client ID
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+# Get Token for API usage
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode('utf-8')
@@ -55,9 +58,11 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+# Get Authorization Header for API
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
+# Get Artist ID from name
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
@@ -71,6 +76,7 @@ def search_for_artist(token, artist_name):
     
     return json_result[0]
 
+# Get Song Recommendations based on artist ID
 def get_songs_recommendations(token, artist_id, min_popularity, max_popularity):
     url = f"https://api.spotify.com/v1/recommendations?limit=100&market=US&seed_artists={artist_id}&min_popularity={min_popularity}&max_popularity={max_popularity}"
     headers = get_auth_header(token)
@@ -78,6 +84,7 @@ def get_songs_recommendations(token, artist_id, min_popularity, max_popularity):
     json_result=json.loads(result.content)
     return json_result['tracks']
 
+# Get top 10 songs by artist ID
 def get_songs_by_artist(token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers = get_auth_header(token)
@@ -85,6 +92,7 @@ def get_songs_by_artist(token, artist_id):
     json_result=json.loads(result.content)["tracks"]
     return json_result
 
+# Get audio features of songs
 def get_song_audio_features(token, song_id):
     url = f"https://api.spotify.com/v1/audio-features?ids={','.join(song_id)}"
     headers = get_auth_header(token)
@@ -125,7 +133,12 @@ def welcome():
 #     return render_template("index.html")
 
 @app.route("/api/v1.0/<artist>/<popularity>")
+
+
 def names(artist, popularity):
+    """Gather necessary recommendations and top songs by artist"""
+
+    # Calculate range for maximum and minimum popularity
     popularity = int(popularity)
     if popularity <10:
         min_popularty = 0
@@ -137,15 +150,21 @@ def names(artist, popularity):
         min_popularty= popularity - 10
         max_popularity = popularity +10
 
+    # Run functions to get token
     token = get_token()
+
+    # Save artist result name
     result = search_for_artist(token, artist)
     artist_id = result["id"]
+
+    # Get song recommendation list
     songs = get_songs_recommendations(token, artist_id, min_popularty, max_popularity)
 
+    # Save empty list to store song list and ID
     recommended_song_list = []
     recommended_song_id = []
 
-    
+    # Loop to save song ID and song recommendation list
     for song in songs:
     # print(song['name'])
     # print(song['id'])
@@ -162,10 +181,12 @@ def names(artist, popularity):
             }
             recommended_song_list.append(dict)
 
+    # Get the top songs and artists and create empty lists to store them
     top_songs_by_artist = get_songs_by_artist(token, artist_id)
     song_list = []
     song_id = []
 
+    # Loop through the list to save recesary information about top songs
     for song in top_songs_by_artist:
     # print(song['name'])
     # print(song['id'])
@@ -181,40 +202,48 @@ def names(artist, popularity):
             }
             song_list.append(dict)
 
+    # Get audio features for recommended songs and top songs
     recommended_features = get_song_audio_features(token, recommended_song_id)
     top_artist_features = get_song_audio_features(token, song_id)
 
 
 
 
-
+    # Create DataFrames
     df_recommended_audiofeatures =pd.DataFrame(recommended_features['audio_features'])
-
     df_top_artist_audiofeatures = pd.DataFrame(top_artist_features['audio_features'])
 
+
+    # Create additional dataFrames and drop duplicates
     df_recommended_song_list = pd.DataFrame(recommended_song_list)
     df_recommended_song_list = df_recommended_song_list.drop_duplicates(subset=['name']).reset_index(drop=True)
 
     df_top_artist_song_list = pd.DataFrame(song_list)
     df_top_artist_song_list = df_top_artist_song_list.drop_duplicates(subset=['name']).reset_index(drop=True)
 
+    # Merge DF
     recommendations_df = pd.merge(df_recommended_song_list, df_recommended_audiofeatures, on ='id')
 
+    # Add in seconds column
     recommendations_df['duration_s'] = recommendations_df['duration_ms'] / 1000
     recommendations_df
 
+    # Merge DF and add seconds column
     top_artist_df = pd.merge(df_top_artist_song_list, df_top_artist_audiofeatures, on ='id')
     top_artist_df['duration_s'] = top_artist_df['duration_ms'] / 1000
     top_artist_df
 
+    # Connect to SQLlite DB
     connection = sqlite3.connect("spotify.sqlite")
 
+    # Save Tables to DB
     recommendation_table = 'recommendation_table'
     recommendations_df.to_sql(recommendation_table, connection, index = False, if_exists='replace')
 
     top_artist_table = 'top_artist_table'
     top_artist_df.to_sql(top_artist_table, connection, index = False, if_exists= 'replace')
 
+    # Commit and Close Changes
     connection.commit()
     connection.close()
 
@@ -222,12 +251,13 @@ def names(artist, popularity):
     session = Session(engine)
 
     """Return a list of all passenger names"""
-    # Query all passengers
+    # Query all top songs and recommendations
     top_songs = session.query(top_artist).all()
     top_recommendations = session.query(recommendation).all()
 
     session.close()
     
+    # Save necessary info for top songs
     top_song_list = []
 
 
@@ -251,6 +281,7 @@ def names(artist, popularity):
 
     top_rec_list = []
 
+    # Save necessary info for top recommendations
     for artist in top_recommendations:
         dict = {
             "song": artist.name,
@@ -271,40 +302,6 @@ def names(artist, popularity):
     data = [top_song_list, top_rec_list]
     return jsonify(data)
 
-
-# @app.route("/api/v1.0/recommendations")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-    
-
-#     session.close()
-
-    
-
-#     for artist in top_recommendations:
-#         dict = {
-#             "song": artist.name,
-#             "artist": artist.artist,
-#             "popularity": artist.popularity,
-#             "danceability": artist.danceability,
-#             "energy": artist.energy,
-#             "loudness": artist.loudness,
-#             "speechiness": artist.speechiness,
-#             "acousticness": artist.acousticness,
-#             "instamentalness": artist.instrumentalness,
-#             "liveness": artist.liveness,
-#             "valence": artist.valence,
-#             "tempo": artist.tempo,
-#             "duration": artist.duration_s
-#         }
-#         top_rec_list.append(dict)
-
-
-#     return jsonify(top_rec_list)
 
 
 if __name__ == '__main__':
